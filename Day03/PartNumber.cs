e9c00c24c9993df4d2f485d5f343febc3a732ff6
spercy-btg <spercy@btg.email>
2023-12-03 16:19:55 -0500

    Day 3, parts 1 and 2

---
+++ Day03/PartNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day03;
+
+internal class PartNumber
+{
+    public PartNumber(int value, int row, int colStart, int colEnd)
+    {
+        this.Value = value; 
+        this.Row = row;
+        this.ColumnStart = colStart;
+        this.ColumnEnd = colEnd;
+    }
+    internal int Value { get; }
+    internal int Row { get; }
+    internal int ColumnStart { get; }
+    internal int ColumnEnd {  get; }
+
+    internal bool IsAdjacent(Point point) => this.CheckNorthWest(point)
+        || this.CheckNorth(point)
+        || this.CheckNorthEast(point)
+        || this.CheckWest(point)
+        || this.CheckEast(point)
+        || this.CheckSouthWest(point)
+        || this.CheckSouth(point)
+        || this.CheckSouthEast(point);
+
+    internal bool CheckNorthWest(Point point) => this.Row == point.Y - 1 && this.ColumnEnd == point.X - 1;
+    internal bool CheckNorth(Point point) => this.Row == point.Y - 1 && (this.ColumnStart <= point.X && this.ColumnEnd >= point.X);
+    internal bool CheckNorthEast(Point point) => this.Row == point.Y - 1 && this.ColumnStart == point.X + 1;
+    internal bool CheckWest(Point point) => this.Row == point.Y && this.ColumnEnd == point.X - 1;
+    internal bool CheckEast(Point point) => this.Row == point.Y && this.ColumnStart == point.X + 1;
+    internal bool CheckSouthWest(Point point) => this.Row == point.Y + 1 && this.ColumnEnd == point.X - 1;
+    internal bool CheckSouth(Point point) => this.Row == point.Y + 1 && (this.ColumnStart <= point.X && this.ColumnEnd >= point.X);
+    internal bool CheckSouthEast(Point point) => this.Row == point.Y + 1 && this.ColumnStart == point.X + 1;
+}
--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,6 +1,35 @@
-﻿File.ReadAllLines("input.txt");
+﻿using Day03;
+using System.Drawing;
+using System.Text.RegularExpressions;
 
-Console.WriteLine($"Part 1:");
-Console.WriteLine($"Part 2:");
+Regex partNumbersRegEx = new Regex(@"\d+", RegexOptions.Compiled);
+Regex symbolRegEx = new Regex("([^\\.|\\d])", RegexOptions.Compiled);
+Regex starRegEx = new Regex(@"\*", RegexOptions.Compiled);
+List<PartNumber> partNumbers = new List<PartNumber>();
+List<Point> symbols = new List<Point>();
+List<Point> stars = new List<Point>();
+
+int rowIdx = 1;
+foreach(string line in File.ReadAllLines("input.txt"))
+{
+    partNumbers.AddRange(partNumbersRegEx.Matches(line).Select(m => new PartNumber(int.Parse(m.Value), rowIdx, m.Index + 1, m.Index + m.Value.Length)));
+    symbols.AddRange(symbolRegEx.Matches(line).Select(m => new Point(m.Index + 1, rowIdx)));
+    stars.AddRange(starRegEx.Matches(line).Select(m => new Point(m.Index + 1, rowIdx)));
+    rowIdx++;
+}
+
+int sumPart1 = symbols.Select(p => partNumbers.Where(pn => pn.IsAdjacent(p)).Sum(pn => pn.Value)).Sum();
+int sumPart2 = 0;
+foreach(Point p in stars)
+{
+    IEnumerable<PartNumber> adjacentPartNumbers = partNumbers.Where(pn => pn.IsAdjacent(p));
+    if(adjacentPartNumbers.Count() == 2)
+    {
+        sumPart2 += (adjacentPartNumbers.First().Value * adjacentPartNumbers.Last().Value);
+    }
+}
+
+Console.WriteLine($"Part 1: {sumPart1}");
+Console.WriteLine($"Part 2: {sumPart2}");
 
 Console.ReadKey();
