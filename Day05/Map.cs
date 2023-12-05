5ac6a81ba880b59618ec64780467380c84f3a81a
spercy-btg <spercy@btg.email>
2023-12-05 13:18:07 -0500

    Day 5, parts 1 and 2

---
+++ Day05/Map.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day05;
+
+internal class Map
+{
+    internal class MapRange
+    {
+        public MapRange(long sourceStart, long destinationStart, long range)
+        {
+            this.SourceStart = sourceStart;
+            this.DestinationStart = destinationStart;
+            this.Range = range;
+        }
+        internal long SourceStart { get; }
+        internal long DestinationStart { get; }
+        internal long Range { get; }
+    }
+    public Map(int order, string name)
+    {
+        this.Order = order;
+        this.Name = name;
+        this.Ranges = new List<MapRange>();
+    }
+    internal int Order { get; } = 0;
+    internal string Name { get; } = string.Empty;
+    internal IList<MapRange> Ranges { get; set; }
+
+    internal void FillSourceToDesination(long source, long destination, long range) => this.Ranges.Add(new MapRange(source, destination, range));
+    internal long GetDestination(long source)
+    {
+        long toReturn = source;
+        MapRange? range = this.Ranges.FirstOrDefault(r => source >= r.SourceStart && source < r.SourceStart + r.Range);
+        if(range != null)
+        {
+            long diff = source - range.SourceStart;
+            toReturn = range.DestinationStart + diff;
+        }
+        return toReturn;
+    }
+}
+
+/*
+    internal IDictionary<long, long> SourceToDestination { get; } = new Dictionary<long, long>();
+    internal void FillSourceToDesination(long source, long destination, long length)
+    {
+        for(int idx = 0; idx < length; idx++)
+        {
+            this.SourceToDestination[source + idx] = destination + idx;
+        }
+    }
+
+    internal long GetDestination(long source) => this.SourceToDestination.ContainsKey(source)
+        ? this.SourceToDestination[source]
+        : source;
+ */
--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,5 +1,82 @@
-﻿File.ReadAllLines("input.txt");
+﻿using Day05;
 
-Console.WriteLine($"Part 1: ");
-Console.WriteLine($"Part 2: ");
+IEnumerable<long> seeds = new List<long>();
+IList<Map> maps = new List<Map>();
+int mapIdx = 0;
+Map currentMap = new Map(0, string.Empty);
+IEnumerable<long> mapSeeds(string line)
+{
+    IEnumerable<long> toReturn = new List<long>();
+    if (!string.IsNullOrEmpty(line))
+    {
+        toReturn = line.Replace("seeds:", "").Trim().Split(" ").Select(s => long.Parse(s.Trim()));
+    }
+
+    return toReturn;
+}
+
+foreach (string line in File.ReadAllLines("input.txt"))
+{
+    if (!string.IsNullOrEmpty(line))
+    {
+        if (line.Contains("seeds:"))
+        {
+            seeds = mapSeeds(line);
+        }
+        else if (line.Contains("map:"))
+        {
+            mapIdx++;
+            currentMap = new Map(mapIdx, line.Replace("map:", "").Trim());
+            maps.Add(currentMap);
+        }
+        else
+        {
+            IEnumerable<long> values = line.Split(" ").Select(v => long.Parse(v.Trim()));
+            if (values.Count() > 2)
+            {
+                currentMap.FillSourceToDesination(values.Skip(1).First(), values.First(), values.Skip(2).First());
+            }
+        }
+    }
+}
+
+long getLocation(long seed)
+{
+    long toReturn = seed;
+    foreach (Map m in maps)
+    {
+        toReturn = m.GetDestination(toReturn);
+    }
+    return toReturn;
+}
+
+long? part1Location = null;
+foreach (long s in seeds)
+{
+    long newLocation = getLocation(s);
+    if (!part1Location.HasValue || newLocation < part1Location)
+    {
+        part1Location = newLocation;
+    }
+}
+
+// Part 2 - takes about three hours to run
+long? part2Location = null;
+IList<long> part2Seeds = new List<long>();
+Parallel.For(0, seeds.Count() / 2, seedIdx =>
+{
+    long start = seeds.Skip(2 * seedIdx).First();
+    long range = seeds.Skip((2 * seedIdx) + 1).First();
+    Parallel.For(0, range, index =>
+    {
+        long newLocation = getLocation(start + index);
+        if (!part2Location.HasValue || newLocation < part2Location)
+        {
+            part2Location = newLocation;
+        }
+    });
+});
+
+Console.WriteLine($"Part 1: {part1Location}");
+Console.WriteLine($"Part 2: {part2Location}");
 Console.ReadLine();
